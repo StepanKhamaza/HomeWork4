aa51cb201fc9c0d31e5921ff3fb2501671d80f62
Stepan Khamaza <khamaza04@mail.ru>
2023-04-18 16:51:45 +0500

    task 1 (version 1)

---
+++ src/main/java/ru/oop/Car.java
@@ -0,0 +1,17 @@
+package ru.oop;
+
+
+public class Car {
+    /**
+     * position - текущее местоположение
+     */
+    public Car(Person person) {
+    }
+
+    /**
+     * перемещение в позицию destination
+     */
+    public void go(Position destination) {
+        // TODO
+    }
+}
